4a68f6767c04adbd410d5666c81c3f26a354c665
Ing. Pablo S. Pannone <pablospannone@gmail.com>
2023-04-05 19:03:51 -0300

    Parte 1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cursos de capacitación en programación</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Cursos de capacitación en programación</h1>
+    <h2>Curso de CSS</h2>
+    <p>Aprenda a diseñar y dar estilo a sitios web utilizando CSS.</p>
+
+    <h2>Curso de HTML</h2>
+    <p>Aprenda a crear sitios web utilizando HTML, la base de la web.</p>
+
+    <h2>Curso de JavaScript</h2>
+    <p>Aprenda a agregar interactividad a su sitio web utilizando JavaScript.</p>
+
+    <h2>Curso de Python</h2>
+    <p>Aprenda a programar en Python, uno de los lenguajes de programación más populares del mundo.</p>
+
+
+    <h2>Curso de CSS</h2>
+    <p>Aprenda a diseñar y dar estilo a sitios web utilizando CSS. <a href="https://ejemplo.com/curso-css">Más
+            información</a></p>
+
+    <h2>Curso de HTML</h2>
+    <p>Aprenda a crear sitios web utilizando HTML, la base de la web. <a href="https://ejemplo.com/curso-html">Más
+            información</a></p>
+
+    <h2>Curso de JavaScript</h2>
+    <p>Aprenda a agregar interactividad a su sitio web utilizando JavaScript. <a href="https://ejemplo.com/curso-js">Más
+            información</a></p>
+
+    <h2>Curso de Python</h2>
+    <p>Aprenda a programar en Python, uno de los lenguajes de programación más populares del mundo. <a
+            href="https://ejemplo.com/curso-python">Más información</a></p>
+
+</body>
+
+</html>
